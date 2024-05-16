41931543c44454eac8f33afc8b118e1b20fe1b69
malursilva <malursilva1712@gmail.com>
2024-05-16 16:40:00 -0300

    refactored tasks form to composition api

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -23,7 +23,7 @@
 
 <script lang="ts">
 
-import { computed, defineComponent } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 import Temporizador from './Temporizador.vue'
 import { useStore } from 'vuex'
 import { key } from '@/store';
@@ -34,27 +34,27 @@ export default defineComponent({
     components: {
         Temporizador
     },
-    data() {
-        return {
-            descricao: '',
-            idProjeto: ''
-        }
-    },
-    methods: {
-        finalizarTarefa(tempoDecorrido: number): void {
-            this.$emit('aoSalvarTarefa', {
+    setup(props, { emit }) {
+        const store = useStore(key)
+        const descricao = ref("")
+        const idProjeto = ref("")
+        const projetos = computed(() => store.state.projeto.projetos)
+
+        const finalizarTarefa = (tempoDecorrido: number): void => {
+            emit('aoSalvarTarefa', {
                 id: new Date().toISOString,
                 duracaoEmSegundos: tempoDecorrido,
-                descricao: this.descricao,
-                projeto: this.projetos.find(proj => proj.id == this.idProjeto)
+                descricao: descricao.value,
+                projeto: projetos.value.find(proj => proj.id == idProjeto.value)
             })
-            this.descricao = ""
+            descricao.value = ""
         }
-    },
-    setup() {
-        const store = useStore(key)
+
         return {
-            projetos: computed(() => store.state.projeto.projetos)
+            descricao,
+            idProjeto,
+            projetos,
+            finalizarTarefa
         }
     }
 });
