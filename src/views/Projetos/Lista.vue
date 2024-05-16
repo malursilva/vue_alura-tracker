6e6a21ea285f9b9baab5f4c5b9ed7168967db482
malursilva <malursilva1712@gmail.com>
2024-05-16 09:53:34 -0300

    refactored project view to nested links

---
+++ src/views/Projetos/Lista.vue
@@ -0,0 +1,50 @@
+<template>
+    <section class="projetos">
+        <router-link to="/projetos/novo" class="button">
+            <span class="icon is-small">
+                <i class="fas fa-plus"></i>
+            </span>
+            <span>Novo Projeto</span>
+        </router-link>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                    <th>
+                        Ações
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="projeto in projetos" :key="projeto.id">
+                    <td>{{ projeto.id }}</td>
+                    <td>{{ projeto.nome }}</td>
+                    <td>
+                        <router-link :to="`/projetos/${projeto.id}`" class="button">
+                            <spam class="icon is-small">
+                                <i class="fas fa-pencil-alt"></i>
+                            </spam>
+                        </router-link>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent } from 'vue'
+import { useStore } from '@/store';
+
+export default defineComponent({
+    name: 'ListaProjetos',
+    setup() {
+        const store = useStore()
+        return {
+            projetos: computed(() => store.state.projetos)
+        }
+    }
+})
+
+</script>
