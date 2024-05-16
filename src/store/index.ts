51b68c689199b14c9dd2947da1942cd2444f8d6e
malursilva <malursilva1712@gmail.com>
2024-05-16 15:58:57 -0300

    refactored mutations and actions to modules

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,54 +1,29 @@
-import IProjeto from "@/interfaces/IProjeto";
 import { createStore, Store, useStore as vuexUseStore } from 'vuex';
 import { InjectionKey } from "vue";
-import { ADICIONA_PROJETO, ADICIONA_TAREFA, ALTERA_PROJETO, ALTERA_TAREFA, DEFINIR_PROJETOS, DEFINIR_TAREFAS, EXCLUIR_PROJETO, NOTIFICAR } from "./tipo-mutacoes";
 import { INotificacao } from "@/interfaces/INotificacao";
-import { ALTERAR_PROJETO, ALTERAR_TAREFA, CADASTRAR_PROJETO, CADASTRAR_TAREFA, OBTER_PROJETOS, OBTER_TAREFAS, REMOVER_PROJETO } from "./tipo-acoes";
-import http from "@/http";
-import ITarefa from "@/interfaces/ITarefa";
+import { EstadoProjeto, projeto } from './modulos/projetos';
+import { EstadoTarefa, tarefa } from './modulos/tarefas';
+import { NOTIFICAR } from './tipo-mutacoes';
 
-interface Estado {
-    projetos: IProjeto[],
-    tarefas: ITarefa[],
-    notificacoes: INotificacao[]
+export interface Estado {
+    notificacoes: INotificacao[],
+    projeto: EstadoProjeto,
+    tarefa: EstadoTarefa
 }
 
 export const key: InjectionKey<Store<Estado>> = Symbol()
 
 export const store = createStore<Estado>({
     state: {
-        projetos: [],
-        tarefas: [],
-        notificacoes: []
+        notificacoes: [],
+        projeto: {
+            projetos: []
+        },
+        tarefa: {
+            tarefas: []
+        }
     },
     mutations: {
-        [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
-            const projeto = {
-                id: new Date().toISOString(),
-                nome: nomeDoProjeto
-            } as IProjeto
-            state.projetos.push(projeto)
-        },
-        [ALTERA_PROJETO](state, projeto: IProjeto) {
-            const index = state.projetos.findIndex(proj => proj.id == projeto.id)
-            state.projetos[index] = projeto
-        },
-        [EXCLUIR_PROJETO](state, idProjeto: string) {
-            state.projetos = state.projetos.filter(proj => proj.id != idProjeto)
-        },
-        [DEFINIR_PROJETOS](state, projetos: IProjeto[]) {
-            state.projetos = projetos
-        },
-        [ADICIONA_TAREFA](state, tarefa: ITarefa) {
-            state.tarefas.push(tarefa)
-        },
-        [ALTERA_TAREFA](state, tarefa: ITarefa) {
-            const index = state.tarefas.findIndex(tar => tar.id == tar.id)
-            state.tarefas[index] = tarefa
-        },
-        [DEFINIR_TAREFAS](state, tarefas: ITarefa[]) {
-            state.tarefas = tarefas
-        },
         [NOTIFICAR](state, novaNotificacao: INotificacao) {
             novaNotificacao.id = new Date().getTime()
             state.notificacoes.push(novaNotificacao)
@@ -58,33 +33,9 @@ export const store = createStore<Estado>({
             }, 2500)
         }
     },
-    actions: {
-        [OBTER_PROJETOS]({ commit }) {
-            http.get('projetos')
-                .then(resposta => commit(DEFINIR_PROJETOS, resposta.data))
-        },
-        [CADASTRAR_PROJETO](contexto, nomeDoProjeto: string) {
-            return http.post('/projetos', { nome: nomeDoProjeto })
-        },
-        [ALTERAR_PROJETO](contexto, projeto: IProjeto) {
-            return http.put(`/projetos/${projeto.id}`, projeto)
-        },
-        [REMOVER_PROJETO]({ commit }, idProjeto: string) {
-            return http.delete(`/projetos/${idProjeto}`)
-                .then(() => commit(EXCLUIR_PROJETO, idProjeto))
-        },
-        [OBTER_TAREFAS]({ commit }) {
-            http.get('tarefas')
-                .then(resposta => commit(DEFINIR_TAREFAS, resposta.data))
-        },
-        [CADASTRAR_TAREFA]({ commit }, tarefa: ITarefa) {
-            return http.post('/tarefas', tarefa)
-                .then(resposta => commit(ADICIONA_TAREFA, resposta.data))
-        },
-        [ALTERAR_TAREFA]({ commit }, tarefa: ITarefa) {
-            return http.put(`/tarefas/${tarefa.id}`, tarefa)
-                .then(() => commit(ALTERA_TAREFA, tarefa))
-        }
+    modules: {
+        projeto,
+        tarefa
     }
 })
 
